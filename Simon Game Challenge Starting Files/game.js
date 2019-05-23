c19bed8a2d6941f6cd80a200780212a9e6bf984a
YoungnohGoh <37110312+YoungnohGoh@users.noreply.github.com>
2019-05-23 16:44:24 +0900

    Add files via upload

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,91 +1,75 @@
 var gamePattern = [];
 var userClickedPattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
-function makeSound(id){
-  switch (id){
-    case 'green':
-        var green = new Audio('sounds/green.mp3');
-        green.play();
-        if (green !== undefined) {
-            green.then(_ => {
-              // Automatic playback started!
-              // Show playing UI.
-            })
-            .catch(error => {
-              // Auto-play was prevented
-              // Show paused UI.
-        });
-        break;
-
-    case 'blue':
-        var blue = new Audio('sounds/blue.mp3');
-        blue.play();
-        if (blue !== undefined) {
-            blue.then(_ => {
-              // Automatic playback started!
-              // Show playing UI.
-            })
-            .catch(error => {
-              // Auto-play was prevented
-              // Show paused UI.
-        });
-        break;
-
-    case 'red':
-        var red = new Audio('sounds/red.mp3');
-        red.play();
-        if (red !== undefined) {
-            red.then(_ => {
-              // Automatic playback started!
-              // Show playing UI.
-            })
-            .catch(error => {
-              // Auto-play was prevented
-              // Show paused UI.
-        });
-        break;
-
-    case 'yellow':
-        var yellow = new Audio('sounds/yellow.mp3');
-        yellow.play();
-        if (yellow !== undefined) {
-            yellow.then(_ => {
-              // Automatic playback started!
-              // Show playing UI.
-            })
-            .catch(error => {
-              // Auto-play was prevented
-              // Show paused UI.
-        });
-        break;
-
-    default:
-        var wrong = new Audio('sounds/wrong.mp3');
-        wrong.play();
-        muted="muted";
-        break;
-  }
-}
+var level = 0;
+var firstCheck = 0;
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  console.log(randomChosenColour);
-  makeSound(randomChosenColour);
+  $("."+randomChosenColour).fadeOut(50).fadeIn(50);
+  animatePress(randomChosenColour);
+  playSound(randomChosenColour);
+  level = level + 1;
+  $("h1").text("level "+level);
 }
-nextSequence();
-// function(name){
 //
-// };
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+};
 
-// $(".btn").on("click", function(){
-//   var userChosenColour = this.id;
-//   userClickedPattern.push(userChosenColour);
-//   console.log(userClickedPattern);
-//   $(this).fadeOut(50);
-//   $(this).fadeIn(50);
-//   makeSound(this.id);
-// });
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");}, 100);
+}
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    if(gamePattern[gamePattern.length-1] === userClickedPattern[currentLevel] && gamePattern.length-1 === currentLevel){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+      userClickedPattern = [];
+    }else{return;}
+  }else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  firstCheck = 0;
+}
+
+$(".btn").on("click", function(){
+  //if user doesn't press a key, click cannot be active.
+  //input the code here.
+  if(userClickedPattern.length-1 < 0 && gamePattern.length-1 < 0){
+    return;
+  }else{
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    $(this).fadeOut(50).fadeIn(50);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+  }
+});
+
+$("body").keypress(function(){
+  ++firstCheck;
+  if(firstCheck===1){
+    nextSequence();
+  }
+});
